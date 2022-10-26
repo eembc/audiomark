a141c3cc69a160d0f3ca0cde0a6c8ee675c52c60
Peter Torelli <peter.j.torelli@gmail.com>
2022-10-26 10:03:25 -0700

    Added CMSIS/NN and a protytpe EE lib

---
+++ libraries/eembc/th_types.h
@@ -0,0 +1,11 @@
+#ifndef __TH_TYPES_H
+#define __TH_TYPES_H
+
+#include "dsp/matrix_functions.h"
+#include "dsp/transform_functions.h"
+
+#define TH_FLOAT32_TYPE                   float
+#define TH_MATRIX_INSTANCE_FLOAT32_TYPE   arm_matrix_instance_f32
+#define TH_MFCC_FFT_INSTANCE_FLOAT32_TYPE arm_rfft_fast_instance_f32
+
+#endif /* __TH_TYPES_H */
