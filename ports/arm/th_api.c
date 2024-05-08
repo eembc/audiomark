c95200b59dce081d107c2cb25723725e8c6becb9
Joseph Yiu <77114984+joseph-yiu@users.noreply.github.com>
2024-05-08 10:32:12 +0100

    Fix incorrect restric as https://github.com/eembc/audiomark/issues/61

--- ports/arm/th_api.c
+++ ports/arm/th_api.c
@@ -1,4 +1,5 @@
 /**
+ * Copyright (C) 2024 SPEC Embedded Group
  * Copyright (C) 2022-2023 EEMBC
  * Copyright (C) 2022-2024 Arm Limited
  *
@@ -79,7 +80,7 @@ th_memcpy(void *restrict dst, const void *restrict src, size_t n)
 }
 
 void *
-th_memmove(void *restrict dst, const void *restrict src, size_t n)
+th_memmove(void * dst, const void * src, size_t n)
 {
     return memmove(dst, src, n);
 }
